9e2d5adab3de4bec734e712346ef19018022a670
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-02-04 20:04:12 -0800

    stat

---
+++ thewildones.py
@@ -0,0 +1,4 @@
+print('******The Wild Ones******')
+
+leads = {"1" : "Johnny", "2" : "Chino", "3" : "Kathie"}
+print(leads)
--- wildone.py
+++
@@ -1,5 +0,0 @@
-print('The Wild Ones')
-
-d = dict()
-d = [1, 'a']
-print(d)
