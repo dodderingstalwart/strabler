7dbb8443c0cddd83e8232a3d9836fb7725c30514
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-02-06 16:09:29 -0800

    stat

--- thewildones.py
+++ thewildones.py
@@ -1,13 +1,22 @@
+class Person:
+    def __init__(self, name, career):
+      self.name = name
+      self.career = career
+
+    def getName(self):
+      print(self.name)
+
+leader = Person("Johnny", "biker")
+enemy = Person("Chino", "rival")
+watress = Person("Kathie", "lady")
 print('******The Wild Ones******')
+
+
 
 # Make a dict() for every character
 leads = {"biker" : "Johnny", "rival" : "Chino", "lady" : "Kathie"}
 
 for l, k in leads.items():
-    print(l, k)
+    print(l, ':', k)
     
-with open("absalom.txt", "r") as fhand:
-    for line in fhand:
-        for word in line.rstrip():
-            print(len(word))
 
