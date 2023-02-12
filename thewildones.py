4928053e4404ecfc08eeb08c6f262bb1af1fbc59
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-02-11 19:25:30 -0800

    stat

--- thewildones.py
+++ thewildones.py
@@ -1,26 +1,20 @@
-class Person:
-    def __init__(self, name, career):
-      self.name = name
-      self.career = career
+#! /usr/bin/env python
 
-    def getName(self):
-      print(self.name)
+def mainTitle():
+    print('************The Wild Ones************')
 
-# main characters
-leader = Person("Johnny", "biker")
-enemy = Person("Chino", "rival")
-watress = Person("Kathie", "lady")
 
-cast = dict()
-cast = [leader.name, enemy.name, watress.name]
-print('******The Wild Ones******')
+def main():
 
-leader.getName()
+    mainTitle()
 
-# Make a dict() for every character
-leads = {"biker" : "Johnny", "rival" : "Chino", "lady" : "Kathie"}
+    # Make a dict() for every character
+    leads = {"biker" : "Johnny", "rival" : "Chino", "lady" : "Kathie"}
 
-for l, k in leads.items():
-    print(l, ':', k)    
+    for l, k in leads.items():
+        print('\t', l, ':', k)
 
-print("What will this program do -_o_-")
+
+if __name__ == '__main__':
+    main()
+
