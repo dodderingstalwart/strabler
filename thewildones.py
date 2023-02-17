70090ef5991ccd94d2b983fbbbecb3544b00b596
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-02-16 20:17:41 -0800

    commandline argument

--- thewildones.py
+++ thewildones.py
@@ -4,6 +4,7 @@
 import webbrowser
 import re
 #import pyperclip
+import sys
 
 # Main Title of program
 def title():
@@ -11,19 +12,23 @@ def title():
     print(''.center(50 ,'*'))
 
 # Retriving an affiliate link website address
-def getWebsite():
-    webSite = input('Enter website: ')
-    return webSite
+#def getWebsite():
+    #webSite = input('Enter website: ')
+    #return webSite
 
 def main():
+    if (len(sys.argv)) < 1:
+        print('Enter a valid website address')
+    web_site1 = sys.argv[1]
     title()
-    web_site = getWebsite()
-    x = re.split("\?", web_site)
+    #web_site2 = getWebsite()
+    x = re.split("\?", web_site1)
     k = x[0]
-    del webSite
+    del web_site1
     #pyperclip.pbcopy(k)
     print('')
     print(k)
+    exit()
 
 if __name__ == '__main__':
     main()
