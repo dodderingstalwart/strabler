62ce58894adcb6d0a55793fd93ab94ce2a5fce91
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-02-16 16:17:59 -0800

    working with pyperclip

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # strabler
 Python 3.11 or later
+
+pyperclip 1.8 or later
--- thewildones.py
+++ thewildones.py
@@ -3,17 +3,25 @@
 
 import webbrowser
 import re
+#import pyperclip
 
+# Main Title of program
 def title():
     print("The Wild Ones".center(50, '*'))
     print(''.center(50 ,'*'))
 
+# Retriving an affiliate link website address
+def getWebsite():
+    webSite = input('Enter website: ')
+    return webSite
+
 def main():
     title()
-    webSite = input('Enter website: ')
+    webSite = getWebsite()
     x = re.split("\?", webSite)
     k = x[0]
     del webSite
+    #pyperclip.pbcopy(k)
     print('')
     print(k)
 
