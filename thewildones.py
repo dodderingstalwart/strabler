4a334bea6401eaf52b397b7c0601113ee82b14d0
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-02-16 21:43:31 -0800

    stat

--- thewildones.py
+++ thewildones.py
@@ -33,11 +33,7 @@ def main():
     for x in web_site1:
         k = re.split("\?", x)
         print('Output Address: ',k[0])
-    #del web_site1
-    #pyperclip.pbcopy(k)
-    #print('')
-    #print('Output Address:', k)
-    #exit()
 
+# call for main 
 if __name__ == '__main__':
     main()
