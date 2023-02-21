5f0df5033253e9a6f2e96a2d8e1e5e1f595f2687
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-02-20 19:02:46 -0800

    stat

--- thewildones.py
+++ thewildones.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
-# thewildone removes affiliate links
+# thewildone.py removes affiliate links
 
 import webbrowser
 import re
-#import pyperclip
 
 def main():
     web_site1 = []
@@ -11,12 +10,13 @@ def main():
     
     while web_name != 'q':
         web_name = input('Enter website address or q to quit: ') 
-        if web_name != 'q':
+        if (web_name != 'q'):
             web_site1.append(web_name)
-    # remove '?' and everything to the right
-    for x in web_site1:
-        k = re.split("\?", x)
-        print('*','Output Address: ',k[0])
+        # remove '?' and everything to the right
+        else:
+            for x in web_site1:
+                k = re.split("\?", x)
+                print('*','Output Address: ',k[0])
 
 # call for main 
 if __name__ == '__main__':
