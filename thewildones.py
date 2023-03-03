26bb283198650648642ad3640edb477dc883ef6b
barda <106283860+dodderingstalwart@users.noreply.github.com>
2023-03-02 19:46:53 -0800

    stat

--- thewildones.py
+++ thewildones.py
@@ -15,21 +15,24 @@ def main():
         if (web_name != 'q'):
             web_store.append(web_name)
         
-        # search and remove affilate link
-        clean_link = re.search("^http(s?):*", web_name)
-        if clean_link:
-            for x in web_store:
-                k = re.split("\?", x)
-                web_fixed.append(k[0])
-        else:
-            web_store.pop()
-            continue
+            # search and remove affilate link
+            clean_link = re.search("^http(s?):*", web_name)
+            if clean_link:
+                for x in web_store:
+                    k = re.split("\?", x)
+                    web_fixed.append(k[0])
+            else:
+                if web_store:
+                    web_store.pop()
+                    continue
+                else:
+                    exit()
 
-        print('***', ' ', k[0])
+            print('***', ' ', k[0])
     
     # Output clean website
     print()
-    print('**', '\n* '.join(set(web_fixed)))
+    print('\n* '.join(set(web_fixed)))
 
 # call for main 
 if __name__ == '__main__':
